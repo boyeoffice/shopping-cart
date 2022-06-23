46ced7b11115932ce8c06ad35c23ea91464fa28b
Olakunle Boye <kunlexzy@gmail.com>
2022-06-23 20:33:49 +0100

    testing migration

--- src/typeorm/migrations/1643481453898-user.ts
+++ src/typeorm/migrations/1643481453898-user.ts
@@ -1,9 +1,36 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableIndex,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
 
 export class user1643481453898 implements MigrationInterface {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  public async up(_queryRunner: QueryRunner): Promise<void> {}
+  public async up(_queryRunner: QueryRunner): Promise<void> {
+    await _queryRunner.createTable(
+      new Table({
+          name: "users",
+          columns: [
+              {
+                  name: "id",
+                  type: "int",
+                  isPrimary: true,
+              },
+              {
+                  name: "name",
+                  type: "varchar",
+              },
+          ],
+      }),
+      true,
+    )
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  public async down(_queryRunner: QueryRunner): Promise<void> {}
+  public async down(_queryRunner: QueryRunner): Promise<void> {
+    await _queryRunner.dropTable("users")
+  }
 }
