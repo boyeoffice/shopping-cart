b2d15d01b3046d8a403b74b89d028ea1ec1d0fd0
Olakunle Boye <kunlexzy@gmail.com>
2022-09-24 21:07:11 +0100

    Added: faker

--- backend/src/typeorm/factories/user.factory.ts
+++ backend/src/typeorm/factories/user.factory.ts
@@ -1,15 +1,21 @@
-import { Faker } from '@faker-js/faker'
-import { define } from "typeorm-seeding";
-
+import { faker} from '@faker-js/faker'
 import { User } from '../entities/user.entity';
 
-define(User, (faker: typeof Faker) => {
-  const gender = faker.datatype.number(1)
-  const firstName = faker.name.firstName(gender)
-  const lastName = faker.name.lastName(gender)
+export const USERS: User[] = [];
+
+function createRandomUser(): User {
 
   const user = new User()
-  user.firstName = firstName;
-  user.lastName = lastName
-  return user
-})
+
+  user.firstName = faker.name.firstName(),
+  user.lastName = faker.name.lastName(),
+  user.isActive = true
+
+  return user;
+}
+
+Array.from({ length: 10 }).forEach(() => {
+  USERS.push(createRandomUser());
+ });
+
+
--- backend/src/typeorm/seeds/UserSeed.ts
+++ backend/src/typeorm/seeds/UserSeed.ts
@@ -2,9 +2,15 @@ import { Factory, Seeder } from "typeorm-seeding";
 import { Connection } from "typeorm";
 
 import { User } from '../entities/user.entity';
+import { USERS } from '../factories/user.factory';
 
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
-    await factory(User)().createMany(15);
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values(USERS)
+      .execute()
   }
 }
